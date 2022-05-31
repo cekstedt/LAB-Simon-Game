449033e679f52680d7e9c8835e9f6667c5d786e8
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-05-31 10:29:18 -0700

    Added game functionality with game.js.

---
+++ game.js
@@ -0,0 +1,62 @@
+// Initialize global variables.
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let userClicks = 0;
+
+// Add event listeners to HTML.
+$("div.btn").click(userClick);
+$(document).keypress(nextSequence);
+
+// Game logic.
+function userClick(e) {
+  let currentColor = e.target.id;
+  flashClass("." + currentColor, "pressed");
+  userClickedPattern.push(currentColor);
+  if (userClickedPattern[userClicks] === gamePattern[userClicks]) {
+    playSound(currentColor);
+    if (userClickedPattern.length === gamePattern.length) {
+      userClicks = 0;
+      userClickedPattern = [];
+      setTimeout(nextSequence, 1000);
+    } else {
+      userClicks++;
+    }
+  } else {
+    startOver();
+  }
+}
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  playSound(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+}
+
+function startOver() {
+  level = 0;
+  userClicks = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  playSound("wrong");
+  flashClass("body", "game-over", 200);
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+}
+
+// Helper functions.
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function flashClass(selector, cls, delay=100) {
+  $(selector).addClass(cls);
+  setTimeout(function() {
+    $(selector).removeClass(cls);
+  }, delay);
+}
